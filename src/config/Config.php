b3d3c1a289573512aedbf6dcb53accb11ecdb2e6
wujianyang <891873508@qq.com>
2020-09-20 09:47:55 +0800

    init

---
+++ src/config/Config.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace weixinSdk\config;
+/**
+ * 对应的wujianyang配置参数
+ */
+class Config
+{
+
+    //curl对接超时时间设置
+    const TIMEOUT = 6;
+    //请求的api地址
+    const URL = 'http://www.wujianyang.com/index/';
+}
